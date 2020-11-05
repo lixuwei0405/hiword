ebe62a78fa41204a15c6cb5882af55a9e5c3d260
lixw <lixuwei0917@163.com>
2020-11-05 17:11:32 +0800

    第二次修改内容Hight.java

--- src/com/atguigu/test/Higit.java
+++ src/com/atguigu/test/Higit.java
@@ -3,5 +3,12 @@ package com.atguigu.test;
 public class Higit {
     public static void main(String[] args) {
         System.out.println("你好  git");
+        Double d = 2.4;
+        System.out.println(d);
+        Integer integer = new Integer(97);
+        Character character =(char) integer.intValue();
+        System.out.println(character);
+
+
     }
 }
